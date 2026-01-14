9b718b5a7a9ce4cea33a5c3bb2c46ad62c8a12db
CatUsb37 <microusbcat5@gmail.com>
2026-01-14 08:57:45 +0200

    Add files via upload

--- realmadrid.js
+++ realmadrid.js
@@ -1,3 +1,7 @@
 function love() {
     alert("Невідомий любив MIRA") // я не несу відповідальність за сайт!!!
 }
+
+function motik() {
+    alert("Motik byv spivakom, meditistom, krivlakoy") // пік
+}
